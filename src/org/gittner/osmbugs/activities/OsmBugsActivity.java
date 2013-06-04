2b39169f13a1349d5d22f24613a561daeded79d3
herrbert74 <herrbert74@gmail.com>
2013-06-04 23:03:53 +0100

    fixed #1 and fixed #2

--- src/org/gittner/osmbugs/activities/OsmBugsActivity.java
+++ src/org/gittner/osmbugs/activities/OsmBugsActivity.java
@@ -19,21 +19,24 @@ import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.ItemizedIconOverlay.OnItemGestureListener;
 import org.osmdroid.views.overlay.OverlayItem;
 
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnLongClickListener;
 import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.widget.ArrayAdapter;
@@ -41,8 +44,8 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class OsmBugsActivity extends Activity implements LocationListener, OnItemGestureListener<Bug>, OnTouchListener{
-		
+public class OsmBugsActivity extends Activity implements LocationListener, OnItemGestureListener<Bug>, OnTouchListener {
+
 	public static int REQUESTCODEBUGEDITORACTIVITY = 1;
 	public static int REQUESTCODESETTINGSACTIVITY = 2;
 
@@ -50,62 +53,67 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 	private static int DIALOGNEWBUG = 2;
 	private static int DIALOGNEWBUGTEXT = 3;
 	private static int DIALOGABOUT = 4;
-	
+
 	public static int INVALIDPLATFORM = -1;
 	public static int KEEPRIGHT = 1;
 	public static int OPENSTREETBUGS = 2;
 	public static int OPENSTREETMAPNOTES = 3;
-		
+
 	private LocationManager locMgr_ = null;
 	private MapView mapView_ = null;
-	
+
 	/* Public static because the Bug Editor gets its Bug by the position in this List */
 	private ArrayList<Bug> bugs_;
 	private ItemizedIconOverlay<Bug> bugOverlay_;
-	
+
 	/* The Location Marker Overlay */
 	private ItemizedIconOverlay<OverlayItem> locationOverlay_;
-	
+
 	/* Used for deciding a long touch on Map */
 	private long mapViewDownTimeStart_;
-	
+
 	/* Used to save the Point where to create the new Bug */
 	private static GeoPoint newBugLocation_;
-	
+	private static PointF startPoint;
+
 	/* Used to save the new Bugs platform */
 	private static int newBugPlatform_;
-	
+
+	private boolean shouldCenterGPS = false;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		/* Enable the Spinning Wheel for undetermined Progress */
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		requestWindowFeature(Window.FEATURE_PROGRESS);
-		
+
 		setContentView(R.layout.activity_osm_bugs);
-		
+
 		/* Init Settings Class */
 		Settings.init(this);
-		
+
 		/* Init the Drawings Class to load all Resources */
 		Drawings.init(this);
 
 		/* Create Bug Overlay */
 		bugs_ = new ArrayList<Bug>();
 		bugOverlay_ = new ItemizedIconOverlay<Bug>(bugs_, Drawings.KeeprightDrawable30, this, new DefaultResourceProxyImpl(this));
-		
+
 		/* Create Position Marker Overlay */
 		locationOverlay_ = new ItemizedIconOverlay<OverlayItem>(new ArrayList<OverlayItem>(), new OnItemGestureListener<OverlayItem>() {
 			@Override
 			public boolean onItemLongPress(int arg0, OverlayItem arg1) {
 				return false;
 			}
+
 			@Override
 			public boolean onItemSingleTapUp(int arg0, OverlayItem arg1) {
 				return false;
-			}}, new DefaultResourceProxyImpl(this));
-		
+			}
+		}, new DefaultResourceProxyImpl(this));
+
 		/* Setup Main MapView */
 		mapView_ = (MapView) findViewById(R.id.mapview);
 		mapView_.setMultiTouchControls(true);
@@ -113,28 +121,28 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 		mapView_.getOverlays().add(bugOverlay_);
 		mapView_.getOverlays().add(locationOverlay_);
 		mapView_.setOnTouchListener(this);
-		
+
 		/* Setup the LocationManager */
 		locMgr_ = (LocationManager) getSystemService(LOCATION_SERVICE);
-		
+
 		/* Try to get a Coarse Location as a starting Point */
 		Location fastLoc = locMgr_.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-		if(fastLoc != null)
+		if (fastLoc != null)
 			onLocationChanged(fastLoc);
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+
 		/* Start Listening to Location updates */
 		locMgr_.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 	}
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
-		
+
 		/* Stop Listening to Location updates */
 		locMgr_.removeUpdates(this);
 	}
@@ -142,45 +150,43 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.osm_bugs, menu);
-		menu.findItem(R.id.center_gps).setChecked(Settings.getCenterGps());	
+		menu.findItem(R.id.center_gps).setChecked(Settings.getCenterGps());
 		return true;
 	}
-	
-	@SuppressWarnings({ "deprecation"})
+
+	@SuppressWarnings({ "deprecation" })
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		if(item.getItemId() == R.id.action_settings){
+		if (item.getItemId() == R.id.action_settings) {
 			/* Start the Settings Activity */
 			Intent i = new Intent(this, SettingsActivity.class);
 			startActivityForResult(i, REQUESTCODESETTINGSACTIVITY);
 			return true;
-		}
-		else if(item.getItemId() == R.id.center_gps){
+		} else if (item.getItemId() == R.id.center_gps) {
 			/* Toggle GPS Map Following */
 			item.setChecked(!item.isChecked());
 			Settings.setCenterGps(!Settings.getCenterGps());
-			
+
 			/* On android API <= 10 The Menu won't display a checkbox so we show a Toast with the Status */
-			if(android.os.Build.VERSION.SDK_INT <= 10){
-				if(Settings.getCenterGps())
+			if (android.os.Build.VERSION.SDK_INT <= 10) {
+				if (Settings.getCenterGps())
 					Toast.makeText(this, getString(R.string.center_on_gps_enabled), Toast.LENGTH_LONG).show();
 				else
 					Toast.makeText(this, getString(R.string.center_on_gps_disabled), Toast.LENGTH_LONG).show();
 			}
 
 			return true;
-		}
-		else if(item.getItemId() == R.id.refresh){
+		} else if (item.getItemId() == R.id.refresh) {
 			/* Update the current Bugs */
 			refreshBugs();
-		}
-		else if(item.getItemId() == R.id.feedback){
+		} else if (item.getItemId() == R.id.center_gps_action) {
+			shouldCenterGPS = true;
+		} else if (item.getItemId() == R.id.feedback) {
 			this.showDialog(DIALOGFEEDBACK);
-		}
-		else if(item.getItemId() == R.id.about) {
+		} else if (item.getItemId() == R.id.about) {
 			this.showDialog(DIALOGABOUT);
 		}
-		
+
 		return super.onOptionsItemSelected(item);
 	}
 
@@ -191,10 +197,15 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 	@Override
 	public void onLocationChanged(Location location) {
 		/* Center Map on GPS Position if activated */
-		if(Settings.getCenterGps()){
+		if (Settings.getCenterGps()) {
 			mapView_.getController().setCenter(new GeoPoint(location));
 		}
-		
+		/* Center Map on GPS Position if button pressed */
+		if (shouldCenterGPS) {
+			shouldCenterGPS = false;
+			mapView_.getController().setCenter(new GeoPoint(location));
+		}
+
 		/* Update the Location Marker */
 		locationOverlay_.removeAllItems();
 		OverlayItem i = new OverlayItem("", "", new GeoPoint(location));
@@ -205,17 +216,17 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 
 	@Override
 	public void onProviderDisabled(String provider) {
-		
+
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
-		
+
 	}
 
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
-		
+
 	}
 
 	@Override
@@ -228,29 +239,28 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 		/* Open the selected Bug in the Bug Editor */
 		Intent i = new Intent(this, BugEditorActivity.class);
 		i.putExtra(BugEditorActivity.EXTRABUG, bug);
-		
+
 		startActivityForResult(i, REQUESTCODEBUGEDITORACTIVITY);
 		return false;
 	}
-	
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if( requestCode == REQUESTCODEBUGEDITORACTIVITY) {
-			if( resultCode == Activity.RESULT_OK)
+		if (requestCode == REQUESTCODEBUGEDITORACTIVITY) {
+			if (resultCode == Activity.RESULT_OK)
 				refreshBugs();
-		}
-		else
+		} else
 			super.onActivityResult(requestCode, resultCode, data);
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@Override
 	public Dialog onCreateDialog(int id) {
-		
-		if(id == DIALOGFEEDBACK){
+
+		if (id == DIALOGFEEDBACK) {
 			/* Create a simple Dialog where a Feedback can be entered */
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			
+
 			final EditText feedbackEditText = new EditText(this);
 			builder.setView(feedbackEditText);
 
@@ -260,60 +270,63 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 				public void onClick(DialogInterface dialog, int which) {
 					onOkClickFeedbackDialog(feedbackEditText.getText().toString());
 					dialog.dismiss();
-				}});
+				}
+			});
 			builder.setNegativeButton(getString(R.string.cancel), new OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					dialog.dismiss();
-				}});
-			
+				}
+			});
+
 			return builder.create();
-		}
-		else if(id == DIALOGNEWBUG) {
+		} else if (id == DIALOGNEWBUG) {
 			/* Ask the User to select a Bug Platform */
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			
+
 			final Spinner spnPlatform = new Spinner(this);
 
 			/* Add the availabe Error Platforms to the Spinner */
 			ArrayList<String> spinnerArray = new ArrayList<String>();
-			for(int i = 0; i != getResources().getStringArray(R.array.new_bug_platforms).length; ++i) {
+			for (int i = 0; i != getResources().getStringArray(R.array.new_bug_platforms).length; ++i) {
 				spinnerArray.add(getResources().getStringArray(R.array.new_bug_platforms)[i]);
 			}
 
-			ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spinnerArray);
+			ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
+					spinnerArray);
 			spnPlatform.setAdapter(spinnerArrayAdapter);
-			
+
 			builder.setView(spnPlatform);
 
 			builder.setMessage(getString(R.string.platform));
 			builder.setPositiveButton(getString(R.string.ok), new OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					if(spnPlatform.getSelectedItemPosition() == 0)
+					if (spnPlatform.getSelectedItemPosition() == 0)
 						newBugPlatform_ = OPENSTREETMAPNOTES;
-					else if(spnPlatform.getSelectedItemPosition() == 1)
+					else if (spnPlatform.getSelectedItemPosition() == 1)
 						newBugPlatform_ = OPENSTREETBUGS;
 					else
 						newBugPlatform_ = INVALIDPLATFORM;
-					
-					if(newBugPlatform_ != INVALIDPLATFORM)
-						showDialog(DIALOGNEWBUGTEXT);	
-					
+
+					if (newBugPlatform_ != INVALIDPLATFORM)
+						showDialog(DIALOGNEWBUGTEXT);
+
 					dialog.dismiss();
-				}});
+				}
+			});
 			builder.setNegativeButton(getString(R.string.cancel), new OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					dialog.dismiss();
-				}});
-			
+				}
+			});
+
 			return builder.create();
-		}
-		else if(id == DIALOGNEWBUGTEXT){
+		} else if (id == DIALOGNEWBUGTEXT) {
 			/* Create a simple Dialog where a Feedback can be entered */
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			
+
 			final EditText newBugText = new EditText(this);
 			builder.setView(newBugText);
 
@@ -321,20 +334,21 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 			builder.setPositiveButton(getString(R.string.ok), new OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					if(!newBugText.getText().toString().equals(""))
-					createBug(newBugPlatform_, newBugText.getText().toString());
-					
+					if (!newBugText.getText().toString().equals(""))
+						createBug(newBugPlatform_, newBugText.getText().toString());
+
 					dialog.dismiss();
-				}});
+				}
+			});
 			builder.setNegativeButton(getString(R.string.cancel), new OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					dialog.dismiss();
-				}});
-			
+				}
+			});
+
 			return builder.create();
-		}
-		else if(id == DIALOGABOUT) {
+		} else if (id == DIALOGABOUT) {
 			/* Show the About Information */
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -343,16 +357,17 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					dialog.dismiss();
-				}});
-			
+				}
+			});
+
 			return builder.create();
 		}
-		
+
 		return super.onCreateDialog(id);
 	}
-	
+
 	private void onOkClickFeedbackDialog(String message) {
-		if(message.length() <= 3000)
+		if (message.length() <= 3000)
 			new SendFeedbackTask(this).execute(message);
 		else
 			Toast.makeText(this, getString(R.string.feedback_message_too_long), Toast.LENGTH_LONG).show();
@@ -361,13 +376,17 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
-		if(v.getId() == R.id.mapview) {
-			if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		if (v.getId() == R.id.mapview) {
+			if (event.getAction() == MotionEvent.ACTION_DOWN) {
 				mapViewDownTimeStart_ = System.currentTimeMillis();
-				newBugLocation_ = (GeoPoint) mapView_.getProjection().fromPixels(event.getX(), event.getY());;
-			}
-			else if( event.getAction() == MotionEvent.ACTION_UP){
-				if(System.currentTimeMillis() - mapViewDownTimeStart_ > 2000){
+				startPoint = new PointF(event.getX(), event.getY());
+				newBugLocation_ = (GeoPoint) mapView_.getProjection().fromPixels(event.getX(), event.getY());
+			} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+				float diff = (float) Math.sqrt(Math.pow(startPoint.x - event.getX(), 2) + Math.pow(startPoint.y - event.getY(), 2));
+				if (diff > 50)
+					mapViewDownTimeStart_ = System.currentTimeMillis();
+			} else if (event.getAction() == MotionEvent.ACTION_UP) {
+				if (System.currentTimeMillis() - mapViewDownTimeStart_ > 2000) {
 					showDialog(DIALOGNEWBUG);
 				}
 			}
@@ -376,21 +395,21 @@ public class OsmBugsActivity extends Activity implements LocationListener, OnIte
 		Log.w("", "Other");
 		return super.onTouchEvent(event);
 	}
-	
+
 	private void createBug(int platform, String text) {
 		Bug bug = null;
-		
-		if(platform == OPENSTREETBUGS){
-			bug = new OpenstreetbugsBug(newBugLocation_.getLatitudeE6() / 1000000.0 , newBugLocation_.getLongitudeE6() / 1000000.0 , "", new ArrayList<Comment>(), -1, Bug.STATE.OPEN);
+
+		if (platform == OPENSTREETBUGS) {
+			bug = new OpenstreetbugsBug(newBugLocation_.getLatitudeE6() / 1000000.0, newBugLocation_.getLongitudeE6() / 1000000.0, "",
+					new ArrayList<Comment>(), -1, Bug.STATE.OPEN);
 			bug.addComment(text);
-		}
-		else if(platform == OPENSTREETMAPNOTES){
-			bug = new OpenstreetmapNote(newBugLocation_.getLatitudeE6() / 1000000.0 , newBugLocation_.getLongitudeE6() / 1000000.0 , "", new ArrayList<Comment>(), -1, Bug.STATE.OPEN);
+		} else if (platform == OPENSTREETMAPNOTES) {
+			bug = new OpenstreetmapNote(newBugLocation_.getLatitudeE6() / 1000000.0, newBugLocation_.getLongitudeE6() / 1000000.0, "",
+					new ArrayList<Comment>(), -1, Bug.STATE.OPEN);
 			bug.addComment(text);
-		}
-		else
+		} else
 			return;
-		
+
 		new BugCreateTask(this).execute(bug);
 	}
 }
